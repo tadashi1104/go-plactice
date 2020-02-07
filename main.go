9fffa2459591a233f1d0d72a2ac7330a9f743882
tadashi.s <tadsuzuki@tech.softbank.co.jp>
2020-02-07 19:17:18 +0900

    20200207-1

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/cmplx"
 	"math/rand"
 	"time"
+)
+
+var c, python, java bool
+var i, j int = 1, 2
+
+// まとめて定義
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
@@ -17,6 +28,42 @@ func main() {
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
 	fmt.Println(split(17))
+	var i int
+	fmt.Println(i, c, python, java)
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+	k := 3
+	d, qython, kava := true, false, "Oh!"
+	fmt.Println(i, j, k, d, qython, kava)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+	var inta int
+	var f float64
+	var boola bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", inta, f, boola, s)
+	typeConversions()
+	constants()
+}
+
+// Pi
+const Pi = 3.14
+
+func constants() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
+
+func typeConversions() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
 }
 
 func add(x, y int) int {
