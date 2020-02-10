a36eb8a66c143d7f123929a8fcbc02f791c93dda
tadashi.s <tadsuzuki@tech.softbank.co.jp>
2020-02-10 19:17:19 +0900

    20200210-1

--- main.go
+++ main.go
@@ -1,81 +1,116 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// 	"math/cmplx"
+// 	"math/rand"
+// 	"time"
+// )
+
+// var c, python, java bool
+// var i, j int = 1, 2
+
+// // まとめて定義
+// var (
+// 	ToBe   bool       = false
+// 	MaxInt uint64     = 1<<64 - 1
+// 	z      complex128 = cmplx.Sqrt(-5 + 12i)
+// )
+
+// func main() {
+// 	fmt.Printf("Hello world\n")
+// 	fmt.Println("The time is", time.Now())
+// 	fmt.Println("My favorite number is", rand.Intn(10))
+// 	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+// 	fmt.Println(math.Pi)
+// 	fmt.Println(add(42, 13))
+// 	a, b := swap("hello", "world")
+// 	fmt.Println(a, b)
+// 	fmt.Println(split(17))
+// 	var i int
+// 	fmt.Println(i, c, python, java)
+// 	var c, python, java = true, false, "no!"
+// 	fmt.Println(i, j, c, python, java)
+// 	k := 3
+// 	d, qython, kava := true, false, "Oh!"
+// 	fmt.Println(i, j, k, d, qython, kava)
+// 	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+// 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+// 	fmt.Printf("Type: %T Value: %v\n", z, z)
+// 	var inta int
+// 	var f float64
+// 	var boola bool
+// 	var s string
+// 	fmt.Printf("%v %v %v %q\n", inta, f, boola, s)
+// 	typeConversions()
+// 	constants()
+// 	practice2()
+// }
+
+// // Pi
+// const Pi = 3.14
+
+// func constants() {
+// 	const World = "世界"
+// 	fmt.Println("Hello", World)
+// 	fmt.Println("Happy", Pi, "Day")
+
+// 	const Truth = true
+// 	fmt.Println("Go rules?", Truth)
+// }
+
+// func typeConversions() {
+// 	var x, y int = 3, 4
+// 	var f float64 = math.Sqrt(float64(x*x + y*y))
+// 	var z uint = uint(f)
+// 	fmt.Println(x, y, z)
+// }
+
+// func add(x, y int) int {
+// 	return x + y
+// }
+
+// func swap(x, y string) (string, string) {
+// 	return y, x
+// }
+
+// func split(sum int) (x, y int) {
+// 	x = sum * 4 / 9
+// 	y = sum - x
+// 	return
+// }
+
+// func practice2() {
+// 	sum := 0
+// 	for i := 0; i < 10; i++ {
+// 		sum += i
+// 	}
+// 	fmt.Println(sum)
+// }
+
 package main
 
 import (
 	"fmt"
-	"math"
-	"math/cmplx"
-	"math/rand"
-	"time"
 )
 
-var c, python, java bool
-var i, j int = 1, 2
+// ErrNegativeSqrt for ErrNegativeSqrt
+type ErrNegativeSqrt float64
 
-// まとめて定義
-var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
-)
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+// Sqrt for Sqrt
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	return 0, nil
+}
 
 func main() {
-	fmt.Printf("Hello world\n")
-	fmt.Println("The time is", time.Now())
-	fmt.Println("My favorite number is", rand.Intn(10))
-	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
-	fmt.Println(math.Pi)
-	fmt.Println(add(42, 13))
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
-	fmt.Println(split(17))
-	var i int
-	fmt.Println(i, c, python, java)
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, j, c, python, java)
-	k := 3
-	d, qython, kava := true, false, "Oh!"
-	fmt.Println(i, j, k, d, qython, kava)
-	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
-	fmt.Printf("Type: %T Value: %v\n", z, z)
-	var inta int
-	var f float64
-	var boola bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", inta, f, boola, s)
-	typeConversions()
-	constants()
-}
-
-// Pi
-const Pi = 3.14
-
-func constants() {
-	const World = "世界"
-	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
-
-	const Truth = true
-	fmt.Println("Go rules?", Truth)
-}
-
-func typeConversions() {
-	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	var z uint = uint(f)
-	fmt.Println(x, y, z)
-}
-
-func add(x, y int) int {
-	return x + y
-}
-
-func swap(x, y string) (string, string) {
-	return y, x
-}
-
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
 }
